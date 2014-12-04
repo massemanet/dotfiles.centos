44d9aed82bead12ac18f7aeb4ce9672c2292f23e
mats cronqvist <masse@klarna.com>
2014-12-05 00:36:17 +0100

    tweak tmux

--- .tmux.sh
+++ .tmux.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-[ $(tput cols) -lt 240 ] && narrowp=1 || narrowp=0
-$(tmux list-session | grep -q attached) && attachedp=1 || attachedp=0
-nsession=$(tmux list-session | wc -l)
-echo $narrowp $attachedp $nsession
+nsession=$(2>/dev/null tmux list-session | wc -l)
+[ $nsession -gt 1 ] && echo "many sessions" && exit 0
+if [ $nsession -eq 0 ]; then
+    tmux new-session -d
+    tmux send-keys -t 0 "emacs" C-m
+    tmux split-window
+    tmux send-keys -t 1 "erl" C-m
+    tmux split-window
+    [ $(tput cols) -lt 240 ] && narrowp="t" || narrowp=""
+    if [ -z $narrowp ]; then
+        tmux select-layout even-vertical
+    else
+        tmux select-layout main-vertical
+    fi
+fi
+
+tmux swap-pane -s 0 -t 1
+tmux list-panes -F "#P:#{pane_current_command}"
+
+$(tmux list-session | grep -q attached) && attachedp="t" || attachedp=""
+[ -z "$attachedp" ] && tmux attach && exit 0
