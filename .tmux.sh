f61674d1de1abacbe1c900acf73e7a3e6a438551
mats cronqvist <masse@klarna.com>
2014-12-05 19:16:55 +0100

    tweak tmux

--- .tmux.sh
+++ .tmux.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+# make sure we have exactly one session
+# if there is no session, start it, and start emacs in pane 0
 nsession=$(2>/dev/null tmux list-session | wc -l)
-[ $nsession -gt 1 ] && echo "many sessions" && exit 0
-if [ $nsession -eq 0 ]; then
+if [ $nsession -gt 1 ]; then
+    echo "many sessions"
+    exit 0
+elif [ $nsession -eq 0 ]; then
     tmux new-session -d
     tmux send-keys -t 0 "emacs" C-m
     tmux split-window
-    tmux send-keys -t 1 "erl" C-m
+    tmux send-keys -t 1 "erl -sname erl@localhost -pa ~/git/eper/ebin" C-m
     tmux split-window
-    [ $(tput cols) -lt 240 ] && narrowp="t" || narrowp=""
-    if [ -z $narrowp ]; then
-        tmux select-layout even-vertical
+fi
+
+# check if the term is narrow/wide, set layout accordingly
+[ $(tput cols) -lt 240 ] && narrowp="t" || narrowp=""
+if [ -n "$narrowp" ]; then
+    tmux set-window-option main-pane-width 81
+    tmux select-layout main-vertical
+else
+    tmux select-layout even-horizontal
+    tmux resize-pane -t 1 -x 81
+fi
+
+# move emacs, if it exists, to pane 0 (narrrow) or pane 1 (wide)
+emacsen=$(tmux list-panes -F "#P:#{pane_current_command}" | grep emacs)
+if [ $(echo $emacsen | wc -l) -eq 1 ]; then
+    emacspane=$(echo $emacsen | cut -f1 -d":")
+    if [ -n "$narrowp" ]; then
+        tmux swap-pane -s $emacspane -t 0
     else
-        tmux select-layout main-vertical
+        tmux swap-pane -s $emacspane -t 1
     fi
 fi
 
-tmux swap-pane -s 0 -t 1
-tmux list-panes -F "#P:#{pane_current_command}"
-
+# attach if needed
 $(tmux list-session | grep -q attached) && attachedp="t" || attachedp=""
-[ -z "$attachedp" ] && tmux attach && exit 0
+[ -z "$attachedp" ] && tmux attach
