af1c81d2e56d451497d49eb8f3bb1607f637a425
mats cronqvist <masse@klarna.com>
2014-12-08 19:38:24 +0100

    merge with mac

--- .tmux.sh
+++ .tmux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# set up three panes; emacs, erl, bash.
+# change layout depending on how wide the terminal is.
 
 # make sure we have exactly one session
-# if there is no session, start it, and start emacs in pane 0
+# if there is no session, start it, and start emacs in pane 0, erl in pane 1
 nsession=$(2>/dev/null tmux list-session | wc -l)
 if [ $nsession -gt 1 ]; then
     echo "many sessions"
@@ -12,22 +14,26 @@ elif [ $nsession -eq 0 ]; then
     tmux split-window
     tmux send-keys -t 1 "erl -sname erl@localhost -pa ~/git/eper/ebin" C-m
     tmux split-window
+    sleep 1
 fi
 
 # check if the term is narrow/wide, set layout accordingly
-[ $(tput cols) -lt 240 ] && narrowp="t" || narrowp=""
+W=$(tmux list-sessions | grep -Eo "\[[0-9]*x" | cut -f1 -d"x" | cut -f2 -d"[")
+[ $W -lt 240 ] && narrowp="t" || narrowp=""
 if [ -n "$narrowp" ]; then
-    tmux set-window-option main-pane-width 81
-    tmux select-layout main-vertical
+    tmux -q set-window-option main-pane-width 81  > /dev/null
+    tmux -q select-layout main-vertical > /dev/null
 else
-    tmux select-layout even-horizontal
-    tmux resize-pane -t 1 -x 81
+    tmux -q select-layout even-horizontal > /dev/null
+    tmux -q resize-pane -t 1 -x 81 > /dev/null
 fi
 
+# panes alist
+panes=$(tmux list-panes -F "#P:#{pane_current_command}\n")
+
 # move emacs, if it exists, to pane 0 (narrrow) or pane 1 (wide)
-emacsen=$(tmux list-panes -F "#P:#{pane_current_command}" | grep emacs)
-if [ $(echo $emacsen | wc -l) -eq 1 ]; then
-    emacspane=$(echo $emacsen | cut -f1 -d":")
+emacspane=$(echo $panes | grep -Eo "[0-9]*:emacs" | cut -f1 -d":" | head -1)
+if [ -n "$emacspane" ]; then
     if [ -n "$narrowp" ]; then
         tmux swap-pane -s $emacspane -t 0
     else
@@ -35,6 +41,10 @@ if [ $(echo $emacsen | wc -l) -eq 1 ]; then
     fi
 fi
 
+# select a bash window, if there is one
+bashpane=$(echo $panes | grep -Eo "[0-9]*:bash" | cut -f1 -d":" | head -1)
+[ -n $bashpane ] && tmux select-pane -t $bashpane
+
 # attach if needed
 $(tmux list-session | grep -q attached) && attachedp="t" || attachedp=""
-[ -z "$attachedp" ] && tmux attach
+[ -n "$attachedp" ] || tmux attach
